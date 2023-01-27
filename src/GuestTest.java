c8d92e0608ed1899290b65dc5c275f4ba45667b8
Alina <alinahussain289@gmail.com>
2023-01-27 01:16:28 -0800

    Initial commit

---
+++ src/GuestTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GuestTest {
+
+	@Test
+	public void test() {
+		GuestTest obj = new GuestTest();
+		String detail = "Guest,Alina,Rabia,090078601,hassan,xyz@kvity.com,"+"Luxury,12,2,30,";
+		
+	}
+
+}
